f22fb07ac59f72fa03ac72ac9422ed19747de033
uyedineniya <73630879+uyedineniya@users.noreply.github.com>
2022-03-21 22:43:38 -0300

    add the springboot project archive to the repo

---
+++ adventureworks-api/src/test/java/com/adventureworksapi/application/WerfamovenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventureworksapi.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerfamovenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
